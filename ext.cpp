a13bc423bff77b7a9c35e3ed10e3a31f0480b8e9
Alexey Mazurenko <zbyshek@gmail.com>
2025-02-07 15:29:56 -0500

    Refactoring for simpler compilation and creating a Python binding

--- ext.cpp
+++ ext.cpp
@@ -3,16 +3,18 @@
  * GRAPHDECO research group, https://team.inria.fr/graphdeco
  * All rights reserved.
  *
- * This software is free for non-commercial, research and evaluation use 
+ * This software is free for non-commercial, research and evaluation use
  * under the terms of the LICENSE.md file.
  *
  * For inquiries contact  george.drettakis@inria.fr
  */
 
 #include <torch/extension.h>
-#include "rasterize_points.h"
 
-PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
+#include "rasterize_points.h"
+#include <pybind11/pybind11.h>
+
+PYBIND11_MODULE(diff_surfel_rasterization_C, m) {
   m.def("rasterize_gaussians", &RasterizeGaussiansCUDA);
   m.def("rasterize_gaussians_backward", &RasterizeGaussiansBackwardCUDA);
   m.def("mark_visible", &markVisible);
--- setup.py
+++ setup.py
@@ -9,9 +9,12 @@
 # For inquiries contact  george.drettakis@inria.fr
 #
 
+import os
+
+os.environ["DISTUTILS_USE_SDK"] = "1"
+
 from setuptools import setup
 from torch.utils.cpp_extension import CUDAExtension, BuildExtension
-import os
 os.path.dirname(os.path.abspath(__file__))
 
 setup(
@@ -20,14 +23,17 @@ setup(
     version='0.0.1',
     ext_modules=[
         CUDAExtension(
-            name="diff_surfel_rasterization._C",
+            name="diff_surfel_rasterization.diff_surfel_rasterization_C",
             sources=[
             "cuda_rasterizer/rasterizer_impl.cu",
             "cuda_rasterizer/forward.cu",
             "cuda_rasterizer/backward.cu",
             "rasterize_points.cu",
             "ext.cpp"],
-            extra_compile_args={"nvcc": ["-I" + os.path.join(os.path.dirname(os.path.abspath(__file__)), "third_party/glm/")]})
+            extra_compile_args={"nvcc": ["-I" + os.path.join(
+                os.path.dirname(os.path.abspath(__file__)),
+                "build/_deps/glm-src/"
+            )]})
         ],
     cmdclass={
         'build_ext': BuildExtension
